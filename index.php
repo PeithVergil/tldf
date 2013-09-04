7ce69967b148caa9f32f03d7f2fcf57c4235018c
Elena Canete <elena.canete@gmail.com>
2013-09-04 23:45:40 +0800

    Added signup form

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	
 	<title>Thai Lady Date Finder</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css" />
+	<link href='http://fonts.googleapis.com/css?family=Linden+Hill' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="style.css" />
 	<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js'></script>
 </head>
@@ -24,7 +25,86 @@
 	<div id="signup">
 		<div class="row">
 		  <div class="col-md-5 col-md-offset-7">
-			<form>
+			<form class="form-horizontal" role="form">
+				<h1>Try For Free</h1>
+				<a href="#" id="fb_login"></a>
+				<p style="margin-bottom: 11px;">&mdash;&mdash;&mdash;&nbsp;or create an account&nbsp;&mdash;&mdash;&mdash;</p>
+
+				<div class="form-group">
+					<label for="username" class="col-lg-3 control-label">Username</label>
+					<div class="col-lg-9">
+					  <input type="text" class="form-control" id="username">
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label for="name" class="col-lg-3 control-label">Name</label>
+					<div class="col-lg-9">
+					  <input type="text" class="form-control" id="name">
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label for="name" class="col-lg-3 control-label">Gender</label>
+					<div class="col-lg-9">
+						<label class="radio-inline">
+						  <input type="radio" id="inlineCheckbox1" value="option1"> Male
+						</label>
+						<label class="radio-inline">
+						  <input type="radio" id="inlineCheckbox2" value="option2"> Female
+						</label>
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label for="name" class="col-lg-3 control-label">Date of Birth</label>
+					<div class="col-lg-9">
+					  <input type="text" class="form-control" id="name">
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label for="name" class="col-lg-3 control-label">Looking for</label>
+					<div class="col-lg-9">
+						<label class="radio-inline">
+						  <input type="radio" id="looking_for" value="option1"> Male
+						</label>
+						<label class="radio-inline">
+						  <input type="radio" id="looking_for" value="option2"> Female
+						</label>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label for="inputEmail1" class="col-lg-3 control-label">Email</label>
+					<div class="col-lg-9">
+					  <input type="email" class="form-control" id="inputEmail1">
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label for="inputEmail1" class="col-lg-3 control-label">Verify Email</label>
+					<div class="col-lg-9">
+					  <input type="email" class="form-control" id="inputEmail1">
+					</div>
+				</div>
+				
+				<div class="form-group">
+					<label for="inputPassword1" class="col-lg-3 control-label">Password</label>
+					<div class="col-lg-9">
+					  <input type="password" class="form-control" id="inputPassword1">
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-lg-12">
+					  <p style="font-size: 0.95em;">By clicking “Start Now!” you agree with the<br><a href="#">Terms amd Conditions</a> and <a href="#">Privacy Policy.</a></p>
+					</div>
+				</div>
+				<div class="form-group">
+					<div class="col-lg-12">
+					  <button type="submit" id="button_start-now">Sign in</button>
+					</div>
+				</div>
 			</form>
 		  </div>
 		</div>
